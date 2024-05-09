490fe504028871846979ba37089b47e978a5d4b6
Augusto <v493707@gmail.com>
2024-05-08 22:20:41 -0300

    att

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const gerenciamentoRouter = require('./routes/excrud')
+const gerenciamentoRouter = require('./routes/crud')
 app.use(gerenciamentoRouter)
 
 function gerenciamento(req, res, next){
